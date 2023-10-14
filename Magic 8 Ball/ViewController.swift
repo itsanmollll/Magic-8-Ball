3a11a22ea2a438ccc9fdffcefa180402d313dff6
Anmol Bhardwaj <anmolbhardwajinv224@gmail.com>
2023-10-14 22:57:16 +0530

    Only Launch Screen lef

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -1,18 +1,19 @@
-//
-//  ViewController.swift
-//  Magic 8 Ball
-//
-//  Created by Angela Yu on 14/06/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
+//  Created by Anmol Bhardwaj on 14/10/2023.
 
 import UIKit
 
 class ViewController: UIViewController {
     
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png"),UIImage(imageLiteralResourceName: "ball1")]
-
-
-
+    let ballArray = [UIImage(imageLiteralResourceName: "ball1"),UIImage(imageLiteralResourceName: "ball2"),UIImage(imageLiteralResourceName: "ball3"),UIImage(imageLiteralResourceName: "ball4"),UIImage(imageLiteralResourceName: "ball5")]
+    
+    @IBOutlet weak var ImageView: UIImageView!
+    
+    
+    @IBAction func askButton(_ sender: Any) {
+        
+        ImageView.image = ballArray.randomElement()
+        
+    }
+    
+    
 }
-
